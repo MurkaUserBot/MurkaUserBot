429bd77060354d5cf023cf9a1ef16332b2546ff3
5050prop <92476958+5050prop@users.noreply.github.com>
2022-05-17 11:12:43 +0300

    Update loader.py

--- friendly-telegram/modules/loader.py
+++ friendly-telegram/modules/loader.py
@@ -107,7 +107,7 @@ class LoaderMod(loader.Module):
     def __init__(self):
         super().__init__()
         self.config = loader.ModuleConfig("MODULES_REPO",
-                                          "https://gitlab.com/friendly-telegram/modules-repo/-/raw/master",
+                                          "https://github.com/MurkaUserBot/official_mods/-/raw/master",
                                           lambda m: self.strings("repo_config_doc", m))
 
     @loader.owner
